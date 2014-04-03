0d9a58f2bb8770d0dcf59c0a8bc003df91538971
Natalie Blackburn & Raouf Nabi <student@epicodus.com>
2014-04-03 11:20:20 -0700

    add CRUD methods for recipe objects

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,59 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render('recipes/index.html.erb')
   end
+
+  def new
+    @recipe = Recipe.new
+    render('recipes/new.html.erb')
+  end
+
+  def create
+    @recipe = Recipe.new(:title => params[:title],
+                         :description => params[:description])
+
+    if @recipe.save
+      flash[:notice] = "Your recipe was successfully saved!"
+       redirect_to("/recipes")
+    else
+      flash[:alert] = "Your recipe was not saved! Try again."
+        render('recipes/new.html.erb')
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render('recipes/edit.html.erb')
+  end
+
+  def update
+    @recipes = Recipe.all
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(:title => params[:title],
+                         :description => params[:description])
+    if @recipe.save
+      flash[:notice] = "Your update was successfully saved!"
+       redirect_to("/recipes")
+    else
+      flash[:alert] = "Your update was not saved! Try again."
+        render('recipes/edit.html.erb')
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render('recipes/show.html.erb')
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    if @recipe.destroy
+      flash[:notice] = "Your recipe was deleted"
+      redirect_to("/")
+    else
+      flash[:alert] = "Your delete failed"
+      redirect_to('/')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Recipes::Application.routes.draw do
+  match('/', {via: :get, to: 'recipes#index'})
   match('recipes', {via: :get, to: 'recipes#index'})
+  match('recipes/new', {:via => :get, to: 'recipes#new'})
+  match('recipes', {:via => :post, to: 'recipes#create'})
+  match('recipes/:id/edit', {via: :get, to: 'recipes#edit'})
+  match('recipes/:id', {via: [:patch, :put], to: 'recipes#update'})
+  match('recipes/:id', {via: :get, to: 'recipes#show'})
+  match('recipes/:id', {via: :delete, to: 'recipes#destroy'})
 end
